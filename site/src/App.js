718550a799fe977ec57dfc8bed66ff7cb49ba46f
Marlon Castro <marloncastro@live.com>
2020-01-06 20:58:07 -0300

    ajustes iniciais

--- site/src/App.js
+++ site/src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 function App() {
+  const [cpf, setCpf] = useState('');
+  async function handleSubmit(event){
+    //Prevenir o funcionamento padrão do formulário    
+    event.preventDefault(); 
+    console.log(cpf);
+
+    const response = await api.post('/sessions', { cpf });
+  }
+
   return (
     <div className="container">
       <div className="content">
         <p>
-          Teste de aplicativo para o PJES.
+          Teste de <strong>Aplicativo</strong>.
         </p>
-        <form>
-          <label htmlFor="email">E-MAIL *</label>
-          <input type="email" id="email"/>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="cpf">CPF *</label>
+          <input 
+            type="text" 
+            id="cpf" 
+            placeholder="digite seu CPF"
+            value={cpf}
+            onChange={event => setCpf(event.target.value)}
+          />
+          <label htmlFor="senha">SENHA *</label>
+          <input 
+            type="password" 
+            id="senha" 
+            placeholder="digite sua senha"
+          />          
+          <button className="btn" type="submit">Entrar</button>
         </form>
       </div>
     </div>
